a7ed2069b1a40dea21a74f08c943b4d626fb186e
bagaslaptop7-dot <bagaslaptop7@gmail.com>
2025-10-08 20:25:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function sayHello(nama) {
   if (nama === "Bagas") {
-    alert("Halo, saya Sobat SMK bernama Bagas!");
+    alert("Halo, saya Sobat SMK Raden Patah Mojosari bernama Bagas!");
   } else if (nama === "Giega") {
-    alert("Halo, saya Sobat SMK bernama Giega!");
+    alert("Halo, saya Sobat SMK Raden Patah Mojosari bernama Giega!");
   } else {
     alert("Halo, saya Sobat SMK!");
   }
